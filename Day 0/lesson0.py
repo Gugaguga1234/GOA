f6a9ea16fff981532d6bf5c62ff5619b5c4b5a62
Gugaguga1234 <gugagioguruli@gmail.com>
2024-01-21 14:58:58 -0500

    first commit

---
+++ Day 0/lesson0.py
@@ -0,0 +1,77 @@
+from turtle import *
+
+#we want to paint a house 
+
+#step 1: draw a square 
+speed(30)
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+
+forward(200)
+
+#end of square 
+
+left(90)
+
+
+
+
+
+    
+#drawing a door 
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+forward(120)   #height of the door 
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+#end of the roof
+#windows
+color("red")
+
+right(240)
+forward(75)
+right(90)
+color("black")
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+
+
+
+
+
+
+
+
+
+exitonclick()
