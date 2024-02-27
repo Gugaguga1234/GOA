362ca2c2e4e0ff1e6616872fa1a175f6f295ff27
Gugaguga1234 <gugagioguruli@gmail.com>
2024-02-27 18:44:14 -0500

    commit

---
+++ Day 04/Homework/lesson4.py
@@ -0,0 +1,44 @@
+name=input("Please enter your name")
+lastname=input("please enter your surname")
+age=input("please enter your age")
+
+print(name surname age)
+
+
+
+
+
+
+
+
+
+
+name = input(x=5)
+secondname = input(y=5)
+print("x+y")
+
+
+
+
+
+name= input("enter your username")
+print("user name is going to about the business")
+print("username is going to be a KETILEBI")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
