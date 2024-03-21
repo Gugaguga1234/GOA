1bfa2ae1e5560dd692fc2505bb407a4597f61503
Gugaguga1234 <gugagioguruli@gmail.com>
2024-03-21 18:03:27 -0400

    commit

---
+++ Day 07/Homework/lesson7.py
@@ -0,0 +1,25 @@
+print = ((variable 1.8) + 32)
+
+
+
+
+
+num1 = input("please enter your first number")
+num2 = int(num1 - 20)
+
+num1 = input("please enter your first number")
+num2 = int(num1 + 11)
+
+length=5
+breadth=6
+area=length*breadth
+print("area of triangle is" , area)
+
+
+number1 = int(input("Enter a number"))
+
+number2 =("is it greater then 100?")
+print("is it completely divisible by 9")
+
+print("is number1 greater or number2")
+print("is number1 and number2 same?")
