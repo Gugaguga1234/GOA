8657551f663c4976462d618866e02760d651347e
Gugaguga1234 <gugagioguruli@gmail.com>
2024-04-04 18:09:59 -0400

    commit

---
+++ Day 09/Classwork/lesson9.py
@@ -0,0 +1,9 @@
+number1 =(345)
+number =(350)
+print("goa")
+
+
+namemum=("her name is mari")
+namedad=("hes name is gio")
+nameson=("hes name is guga")
+namecat=("hes name is mortis")
---
+++ Day 09/Homework/lesson9.py
@@ -0,0 +1,3 @@
+print("if it is more then 10000 were you learning in goa") 
+print("if it is between 1000-10000 you mid")
+print("if it is less then 1000 you should join in goa you matrica")
