0119691891f25f727c7840e9a7ba9947301ccbd6
Gugaguga1234 <gugagioguruli@gmail.com>
2024-04-18 20:35:36 -0400

    commit

---
+++ Day 11/Classwork/lesson11.py
@@ -0,0 +1,4 @@
+name = int("please enter your name")
+lastname = int("please enter your last name")
+age =  int("please enter your age")
+location = int("please enter your location")
---
+++ Day 11/Homework/lesson11.py
@@ -0,0 +1,22 @@
+print("html is used for structure a web page and its content")
+print("<html lang="en">")
+
+start = int(("please enter your car model name"))
+business =  int(("enter amount of prize of 1car"))
+dealarship = int(("please enter your dearships name"))
+
+
+<p>jungle book the book anout the life adventure of salamura fist tree<p/>
+<h1>it is so good books and they are all interesting
+
+print = int(("create paragraph"))
+print("World War II conjures images of genocide, biological warfare, and ceaseless violence. This narrative, while undeniably crucial, does not address the histories of colonized peoples during this time. Struggles during the war went beyond the battlefield and mainstream recollections. Contrasting food inventions between colonial powers and their holdings during World War II illuminates these  histories.
+Entering World War II, America was emerging as a world power. Their geographic isolation from World War I allowed them to recover and prosper in the interwar era, and food invention during the Depression carried into World War II. Food items such as SPAM and M&Ms profoundly impacted the military and were later incorporated into some American dishes after the war. The relationship between entrepreneurs and the American military was vital to food invention at the time, as resources were allocated towards the war effort. The Hershey chocolate company also became a chocolate provider for the military, and food science led to popular American snack foods.
+The American military introduced their own food inventions to subjugated places, spurring more food innovation in places they had colonized and waged war. For people feeling the strain of resources and starvation, such as Hawaii, the introduction of SPAM became a key part of their history during the war and would continue to sustain them after. Korea and Okinawa also adopted the luncheon meat to make dishes such as SPAM musubi and Poku Tamago, despite its introduction's grim history. SPAM dishes are still enjoyed today, though there have been movements to acknowledge its colonial connections while reclaiming it. As for the Japanese Imperial Army, new Westernized food inventions—a way to illustrate the power and strengthen the army—played a crucial role in developing modern Japanese cuisine.
+Europe’s food invention was less connected to the military. Homefront food invention was government sponsored, with the dishes of Eintpof and Woolton Pie reflecting the degree of ability of each country to effectively feed and keep morale up. Despite times of extreme food shortages and rationing, they were better off than others as citizens of these powers. If the dishes are enjoyed today, they are enjoyed with either little acknowledgment of the histories or as a symbol of past hardships as opposed to continual struggle.
+Contrasting sharply was the Bengal Famine. As a British colony, India was at the whims of Britain at the time of the war and suffered millions of deaths. Despite the famine, food invention was still possible during these times, showing perseverance. Similar themes emerged in other parts of Asia, specifically Southeast Asia, such as the Philippines and Thailand underwent shortages and famines, leading to inventive dishes and ingredients that took advantage of the few resources available, such as pad Thai and banana ketchup. For the Philippines, food scientists like María Orosa e Ylagan used innovative ideas to simultaneously combat food shortages and support the country’s movement toward self-sufficiency. In Thailand, food also meant independence, as Prime Minister Plaek Phibunsongkhram sought to develop a Thai national identity.
+Though colonized people were at every disadvantage, they still showed ingenuity in the face of starvation. Such ingenuity did not eradicate food insecurity or the threat of starvation, however. Many of the food inventions are still enjoyed today but carry the weight of colonization. In many cases, while colonized spaces struggled with food scraps, their imperial heads ran government-sponsored programs to keep their citizens fed. The differences in how colonizing and colonized spaces dealt with the food insecurity brought on by World War II reveal the glaring inequality in homefront experiences.
+Picture")
+
+
+
