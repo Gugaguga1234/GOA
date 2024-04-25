87fee533ee5b6d69656ce7b96290a88804dcb4a6
Gugaguga1234 <gugagioguruli@gmail.com>
2024-04-25 16:33:59 -0400

    commit

--- Day 12/Homework/lesson12.py
+++ Day 12/Homework/lesson12.py
@@ -1,18 +1,22 @@
-for num in range(11):
-    if num % 2 != 0:
-     print(num)
-for num in range(1, 51):
-    if num % 5 == 0:
-        print(num)
+# for num in range(11):
+#     if num % 2 != 0:
+#      print(num)
+# for num in range(1, 51):
+#     if num % 5 == 0:
+#         print(num)
 
-num = int(input("Enter an integer: "))
+# num = int(input("Enter an integer: "))
 
-for i in range(num + 1):
-    print(i)
+# for i in range(num + 1):
+#     print(i)
 
-for num in range(51):
-    print(num)
+# for num in range(51):
+#     print(num)
 
-total_sum = sum(range(51))
+#
+total = 0
+for i in range(1,51):
+    total+= i
+    print(total)
 
-print("The sum of numbers from 0 to 50 is:", total_sum)
+
